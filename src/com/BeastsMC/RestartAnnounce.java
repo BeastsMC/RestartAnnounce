eb4e7c973a901d191f0b141e15785610ff7f5cc7
Zane <beastsmc@gmail.com>
2013-06-18 15:35:33 -1000

    Initial update

---
+++ src/com/BeastsMC/RestartAnnounce.java
@@ -0,0 +1,59 @@
+package com.BeastsMC;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RestartAnnounce extends JavaPlugin {
+	public Logger log;
+	public boolean restartScheduled = false;
+	public ScheduledRestart restart = null;
+	public void onEnable() {
+		log = this.getLogger();
+		getServer().getPluginManager().registerEvents(new RALoginListener(this), this);
+		getCommand("sr").setExecutor(this);
+		getCommand("cancelrestart").setExecutor(this);
+	}
+	public boolean onCommand(CommandSender sender, Command command, String cmd, String[] args) {
+		if(cmd.equalsIgnoreCase("cancelrestart")) {
+			if(restartScheduled) {
+				cancelRestart();
+				restartScheduled = false;
+				log.info("Cancelled the scheduled restart");
+				sender.sendMessage(ChatColor.BLUE + "Cancelled the scheduled restart");
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.RED + "No server restart scheduled");
+				return true;
+			}
+		} else {
+			if(restartScheduled) {
+				sender.sendMessage(ChatColor.RED + "Server restart already scheduled");
+				return true;
+			}
+			if(args.length > 0) {
+				Integer seconds;
+				try {
+					seconds = Integer.parseInt(args[0]);
+				} catch(NumberFormatException e) {
+					sender.sendMessage(ChatColor.RED + "'"  + args[0] +"' is not a number.");
+					return false;
+				}
+				restart = new ScheduledRestart(this, seconds);
+				restartScheduled = true;
+				log.info("Scheduled restart to occur in " + seconds + " seconds");
+				sender.sendMessage(ChatColor.BLUE + "Scheduled restart to occur in " + seconds + " seconds");
+				return true;
+			}
+			return false;
+		}
+	}
+	private void cancelRestart() {
+		restart.destroy();
+		restart = null;
+	}
+
+}
---
+++ src/com/BeastsMC/ScheduledRestart.java
@@ -0,0 +1,69 @@
+package com.BeastsMC;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
+public class ScheduledRestart extends BukkitRunnable {
+	private Integer secondsUntilRestart;
+	private final RestartAnnounce plugin;
+	private final Objective objective;
+	private final Scoreboard board;
+	public ScheduledRestart(RestartAnnounce main, Integer seconds) {
+		plugin = main;
+		secondsUntilRestart = seconds;
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		board = manager.getNewScoreboard();
+		objective = board.registerNewObjective("restarttime", "dummy");
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+		objective.setDisplayName("Time Until Restart");
+		for(Player online : Bukkit.getOnlinePlayers()) {
+			online.setScoreboard(board);
+		}
+		this.runTaskTimer(plugin, 20, 20);
+	}
+	@Override
+	public void run() {
+		secondsUntilRestart--;
+		if(secondsUntilRestart==0) {
+			plugin.log.info("Running shut down");
+			Bukkit.shutdown();
+			this.cancel();
+		}
+		this.updateDisplays();
+	}
+	private void updateDisplays() {
+		OfflinePlayer restartLine;
+		Score score;
+		if(secondsUntilRestart==59) {
+			restartLine = Bukkit.getOfflinePlayer(ChatColor.RED + "Minutes:");
+			board.resetScores(restartLine);
+		}
+		if(secondsUntilRestart>59) {
+			restartLine = Bukkit.getOfflinePlayer(ChatColor.RED + "Minutes:");
+			Integer minutesToRestart = secondsUntilRestart/60;
+			score = objective.getScore(restartLine);
+			score.setScore(minutesToRestart);
+
+		} else {
+			restartLine = Bukkit.getOfflinePlayer(ChatColor.RED + "Seconds:");
+			score = objective.getScore(restartLine);
+			score.setScore(secondsUntilRestart);
+		}
+	}
+	public void destroy() {
+		this.cancel();
+		objective.unregister();
+	}
+	public Scoreboard getScoreboard() {
+		return board;
+	}
+}
