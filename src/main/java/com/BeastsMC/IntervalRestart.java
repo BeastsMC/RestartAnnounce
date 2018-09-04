df499d7a81a8ec80b5f1da2a0f0567a3a1566d00
Alexander Lamprecht <a.lamprecht@tup.com>
2018-09-04 15:25:06 +0200

    format sources

--- src/main/java/com/BeastsMC/IntervalRestart.java
+++ src/main/java/com/BeastsMC/IntervalRestart.java
@@ -3,30 +3,33 @@ package com.BeastsMC;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class IntervalRestart extends BukkitRunnable {
-	private Integer secondsUntilRestart;
-	private final RestartAnnounce plugin;
-	public IntervalRestart(RestartAnnounce main, Integer seconds) {
-		plugin = main;
-		secondsUntilRestart = seconds;
-		this.runTaskTimer(plugin, 20, 20);
-	}
-	@Override
-	public void run() {
-		if(secondsUntilRestart<=3600) {
-			plugin.restart = new ScheduledRestart(plugin, secondsUntilRestart);
-			plugin.restartScheduled = true;
-			plugin.intervalRestart = null;
-			this.cancel();
-		}
-		secondsUntilRestart--;
-	}
-	public String getFormatedTime() {
-		if(secondsUntilRestart<60) {
-			return secondsUntilRestart + " seconds";
-		} else if(secondsUntilRestart<3600) {
-			return (double)Math.round(secondsUntilRestart/60 * 1000) / 1000 + " minutes";
-		} else {
-			return (double)Math.round(secondsUntilRestart/3600 * 1000) / 1000 + " hours";
-		}
-	}
+    private Integer secondsUntilRestart;
+    private final RestartAnnounce plugin;
+
+    public IntervalRestart(RestartAnnounce main, Integer seconds) {
+        plugin = main;
+        secondsUntilRestart = seconds;
+        this.runTaskTimer(plugin, 20, 20);
+    }
+
+    @Override
+    public void run() {
+        if (secondsUntilRestart <= 3600) {
+            plugin.restart = new ScheduledRestart(plugin, secondsUntilRestart);
+            plugin.restartScheduled = true;
+            plugin.intervalRestart = null;
+            this.cancel();
+        }
+        secondsUntilRestart--;
+    }
+
+    public String getFormatedTime() {
+        if (secondsUntilRestart < 60) {
+            return secondsUntilRestart + " seconds";
+        } else if (secondsUntilRestart < 3600) {
+            return (double) Math.round(secondsUntilRestart / 60 * 1000) / 1000 + " minutes";
+        } else {
+            return (double) Math.round(secondsUntilRestart / 3600 * 1000) / 1000 + " hours";
+        }
+    }
 }
--- src/main/java/com/BeastsMC/RALoginListener.java
+++ src/main/java/com/BeastsMC/RALoginListener.java
@@ -7,15 +7,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class RALoginListener implements Listener {
-	private final RestartAnnounce plugin;
-	public RALoginListener(RestartAnnounce main) {
-		plugin = main;
-	}
-	@EventHandler(priority=EventPriority.MONITOR)
-	public void onPlayerJoin(PlayerJoinEvent playerjoin) {
-		if(plugin.restart != null && plugin.restartScheduled) {
-			Player p = playerjoin.getPlayer();
-			p.setScoreboard(plugin.restart.getScoreboard());
-		}
-	}
+    private final RestartAnnounce plugin;
+
+    public RALoginListener(RestartAnnounce main) {
+        plugin = main;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent playerjoin) {
+        if (plugin.restart != null && plugin.restartScheduled) {
+            Player p = playerjoin.getPlayer();
+            p.setScoreboard(plugin.restart.getScoreboard());
+        }
+    }
 }
